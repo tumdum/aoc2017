fce0a26c29b7b3dc0dad5930d32b26c655b8b33d
Tomasz Kłak <tomasz@tomaszklak.pl>
2017-12-22 07:10:09 +0100

    Day 21 part 2 in rust

--- 22/main.rs
+++ 22/main.rs
@@ -1,11 +1,11 @@
 use std::io::{BufRead,BufReader,Read};
-use std::collections::HashSet;
+use std::collections::{HashMap,HashSet};
 
 type Pos = (i64, i64);
 
 enum Rotation { Left, Right }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum Orientation { Up, Down, Left, Right }
 
 impl Orientation {
@@ -28,9 +28,18 @@ impl Orientation {
                 .rotate(Rotation::Left).rotate(Rotation::Left).rotate(Rotation::Left),
         }
     }
+
+    fn reverse(&self) -> Orientation {
+        match self {
+            &Orientation::Up => Orientation::Down,
+            &Orientation::Down => Orientation::Up,
+            &Orientation::Left => Orientation::Right,
+            &Orientation::Right => Orientation::Left,
+        }
+    }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Carrier {
     pos: Pos,
     orientation: Orientation,
@@ -44,16 +53,68 @@ impl Carrier {
     fn move_forward(&mut self) {
         self.pos = self.orientation.move_pos(self.pos);
     }
+
+    fn reverse(&mut self) {
+        self.orientation = self.orientation.reverse();
+    }
+}
+
+#[derive(Clone,PartialEq)]
+enum Flag { Clean, Weakened, Infected, Flagged }
+
+impl Flag {
+    fn next(&self) -> Flag {
+        match self {
+            &Flag::Clean => Flag::Weakened,
+            &Flag::Weakened => Flag::Infected,
+            &Flag::Infected => Flag::Flagged,
+            &Flag::Flagged => Flag::Clean,
+        }
+    }
 }
 
 #[derive(Clone)]
 struct Grid {
-    infected: HashSet<Pos>,
+    states: HashMap<Pos, Flag>,
     infections: usize,
 }
 
 impl Grid {
+    fn new(s: HashSet<Pos>) -> Grid {
+        // Grid{infected: s, infections: 0}
+        let mut m = HashMap::new();
+        for p in s {
+            m.insert(p, Flag::Infected);
+        }
+        Grid{states: m, infections: 0}
+    }
+
+    fn get_flag(&self, pos: &Pos) -> Flag {
+        self.states.get(pos).cloned().unwrap_or(Flag::Clean)
+    }
+
+    fn touch(&mut self, pos: &Pos) {
+        let current_flag = self.get_flag(pos);
+        let next_flag = current_flag.next();
+        if next_flag == Flag::Infected {
+            self.infections += 1;
+        }
+        self.states.insert(pos.clone(), next_flag);
+    }
+
     fn burst(&mut self, mut c: Carrier) -> Carrier {
+        let current_flag = self.get_flag(&c.pos);
+        match current_flag {
+            Flag::Clean => c.rotate(Rotation::Left),
+            Flag::Weakened => {},
+            Flag::Infected => c.rotate(Rotation::Right),
+            Flag::Flagged => c.reverse(),
+        }
+
+        self.touch(&c.pos);
+        c.move_forward();
+
+        /*
         let current_infected = self.infected.contains(&c.pos);
         if current_infected {
             c.rotate(Rotation::Right);
@@ -64,6 +125,7 @@ impl Grid {
             self.infections += 1;
         }
         c.move_forward();
+        */
         c
     }
 }
@@ -89,15 +151,13 @@ fn solve_a(mut g: Grid, mut c: Carrier, steps: usize) {
     for _ in 0..steps {
         c = g.burst(c);
     }
-    println!("{} => {}", steps, g.infections);
+    println!("{:10} => {}", steps, g.infections);
 }
 
 fn main() {
     let (infected, start) = parse(std::io::stdin());
-    println!("{:?} {:?}", infected, start);
-    let grid = Grid{infected: infected, infections: 0};
+    let grid = Grid::new(infected);
     let carrier = Carrier{pos: start, orientation: Orientation::Up};
-    // solve_a(grid.clone(), carrier.clone(), 7);
-    // solve_a(grid.clone(), carrier.clone(), 70);
-    solve_a(grid.clone(), carrier.clone(), 10000);
+    solve_a(grid.clone(), carrier.clone(), 100);
+    solve_a(grid.clone(), carrier.clone(), 10000000);
 }
