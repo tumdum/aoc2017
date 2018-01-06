3f5172004f1bc6f785bde6a5d3042a945a439f06
Tomasz Kłak <tomasz@tomaszklak.pl>
2018-01-06 16:04:45 +0100

    fixed rustc complaints

--- 01/main.rs
+++ 01/main.rs
@@ -4,7 +4,7 @@ fn parse(input: &str) -> Vec<u32> {
     input.chars().map(|c| c.to_digit(10).unwrap()).collect()
 }
 
-fn sum(input: &[u32]) -> u32 {
+fn solution_a(input: &[u32]) -> u32 {
     let ret = input
         .windows(2)
         .map(|w| if w[0] == w[1] { w[0] } else { 0 })
@@ -20,7 +20,7 @@ fn pos(size: usize, pos: usize) -> usize {
     (pos + size/2) % size
 }
 
-fn sumB(input: &[u32]) -> u32 {
+fn soltution_b(input: &[u32]) -> u32 {
     let size = input.len();
     input
         .iter()
@@ -32,9 +32,9 @@ fn sumB(input: &[u32]) -> u32 {
 fn main() {
     let mut input = std::io::stdin();
     let mut buf = String::new();
-    input.read_to_string(&mut buf);
-    println!("{}", sum(&parse(buf.trim())));
-    println!("{}", sumB(&parse(buf.trim())));
+    input.read_to_string(&mut buf).unwrap();
+    println!("{}", solution_a(&parse(buf.trim())));
+    println!("{}", soltution_b(&parse(buf.trim())));
 }
 
 #[test]
@@ -47,17 +47,17 @@ fn parse_test() {
 
 #[test]
 fn parse_sum() {
-    assert_eq!(3, sum(&[1,1,2,2]));
-    assert_eq!(4, sum(&[1,1,1,1]));
-    assert_eq!(0, sum(&[1,2,3,4]));
-    assert_eq!(9, sum(&[9,1,2,1,2,1,2,9]));
+    assert_eq!(3, solution_a(&[1,1,2,2]));
+    assert_eq!(4, solution_a(&[1,1,1,1]));
+    assert_eq!(0, solution_a(&[1,2,3,4]));
+    assert_eq!(9, solution_a(&[9,1,2,1,2,1,2,9]));
 }
 
 #[test]
-fn parse_sumB() {
-    assert_eq!(6, sumB(&[1,2,1,2]));
-    assert_eq!(0, sumB(&[1,2,2,1]));
-    assert_eq!(4, sumB(&[1,2,3,4,2,5]));
-    assert_eq!(12, sumB(&[1,2,3,1,2,3]));
-    assert_eq!(4, sumB(&[1,2,1,3,1,4,1,5]));
+fn parse_soltution_b() {
+    assert_eq!(6, soltution_b(&[1,2,1,2]));
+    assert_eq!(0, soltution_b(&[1,2,2,1]));
+    assert_eq!(4, soltution_b(&[1,2,3,4,2,5]));
+    assert_eq!(12, soltution_b(&[1,2,3,1,2,3]));
+    assert_eq!(4, soltution_b(&[1,2,1,3,1,4,1,5]));
 }
--- 25/main.rs
+++ 25/main.rs
@@ -1,22 +1,74 @@
-#[derive(Debug)]
+use std::collections::HashMap;
+use std::cell::*;
+
+enum Direction { Left, Right }
+
+#[derive(Hash,PartialEq,Eq)]
 enum State { A, B, C, D, E, F }
 
-#[derive(Debug)]
+struct Description {
+    off_action: Action,
+    on_action: Action,
+}
+
+fn i2b(i: usize) -> bool { i == 1 }
+
+impl Description {
+    fn new(v_off: usize, d_off: Direction, s_off: State, v_on: usize, d_on: Direction, s_on: State) -> Self {
+        Description{
+            off_action: Action::new(i2b(v_off), d_off, s_off),
+            on_action: Action::new(i2b(v_on), d_on, s_on),
+        }
+    }
+}
+
+struct Action {
+    value_to_set: bool,
+    direction_to_take: Direction,
+    state_to_transition: State,
+}
+
+impl Action {
+    fn new(value_to_set: bool, direction_to_take: Direction, state_to_transition: State) -> Self {
+        Action{value_to_set, direction_to_take, state_to_transition}
+    }
+}
+
 struct Machine {
     tape: Vec<bool>,
     state: State,
     position: usize,
     checksum_steps: usize,
+    descriptions: HashMap<State, RefCell<Description>>,
 }
 
 impl Machine {
     fn new() -> Self {
+        use Direction::*;
+        use State::*;
+
+        let a_desc = RefCell::new(Description::new(1, Right, B,  0, Left, B));
+        let b_desc = RefCell::new(Description::new(1, Left, C,   0, Right, E));
+        let c_desc = RefCell::new(Description::new(1, Right, E,  0, Left, D));
+        let d_desc = RefCell::new(Description::new(1, Left, A,   1, Left, A));
+        let e_desc = RefCell::new(Description::new(0, Right, A,  0, Right, F));
+        let f_desc = RefCell::new(Description::new(1, Right, E,  1, Right, A));
+
+        let mut descriptions = HashMap::new();
+        descriptions.insert(A, a_desc);
+        descriptions.insert(B, b_desc);
+        descriptions.insert(C, c_desc);
+        descriptions.insert(D, d_desc);
+        descriptions.insert(E, e_desc);
+        descriptions.insert(F, f_desc);
+
         let steps = 12683008;
         Machine{
             tape: vec![false; (steps+1)*2],
             state: State::A,
             position: steps+1,
             checksum_steps: steps,
+            descriptions: descriptions,
         }
     }
 
@@ -32,12 +84,31 @@ impl Machine {
         self.tape[self.position] = val;
     }
 
-    fn move_left(&mut self) {
-        self.position -= 1;
+    fn move_to(&mut self, direction: Direction) {
+        match direction {
+            Direction::Left => self.position -= 1,
+            Direction::Right => self.position += 1,
+        }
     }
 
-    fn move_right(&mut self) {
-        self.position += 1;
+    fn run(&mut self) -> usize {
+        for _ in 0..self.checksum_steps {
+            let value = self.current_value();
+            let action : Ref<_> = if value {
+                self.descriptions[&self.state].borrow()
+            } else {
+                self.descriptions[&self.state].borrow()
+            };
+            if value {
+                self.execute(action);
+            } else {
+                self.execute(action);
+            }
+        }
+        self.checksum()
+    }
+
+    fn execute(&self, d: Ref<Description>) {
     }
 
     fn run_till_checksum(&mut self) -> usize {
@@ -46,66 +117,66 @@ impl Machine {
                 State::A => {
                     if !self.current_value()  {
                         self.write(true);
-                        self.move_right();
+                        self.move_to(Direction::Right);
                         self.state = State::B;
                     } else {
                         self.write(false);
-                        self.move_left();
+                        self.move_to(Direction::Left);
                         self.state = State::B;
                     }
                 },
                 State::B => {
                     if !self.current_value() {
                         self.write(true);
-                        self.move_left();
+                        self.move_to(Direction::Left);
                         self.state = State::C;
                     } else {
                         self.write(false);
-                        self.move_right();
+                        self.move_to(Direction::Right);
                         self.state = State::E;
                     }
                 },
                 State::C => {
                     if !self.current_value() {
                         self.write(true);
-                        self.move_right();
+                        self.move_to(Direction::Right);
                         self.state = State::E;
                     } else {
                         self.write(false);
-                        self.move_left();
+                        self.move_to(Direction::Left);
                         self.state = State::D;
                     }
                 },
                 State::D => {
                     if !self.current_value() {
                         self.write(true);
-                        self.move_left();
+                        self.move_to(Direction::Left);
                         self.state = State::A;
                     } else {
                         self.write(true); // TODO: not needed
-                        self.move_left();
+                        self.move_to(Direction::Left);
                         self.state = State::A;
                     }
                 },
                 State::E => {
                     if !self.current_value() {
                         self.write(false); // TODO: not needed
-                        self.move_right();
+                        self.move_to(Direction::Right);
                         self.state = State::A;
                     } else {
                         self.write(false);
-                        self.move_right();
+                        self.move_to(Direction::Right);
                         self.state = State::F;
                     }
                 },
                 State::F => {
                     if !self.current_value() {
                         self.write(true);
-                        self.move_right();
+                        self.move_to(Direction::Right);
                         self.state = State::E;
                     } else {
                         self.write(true); // TODO: not needed;
-                        self.move_right();
+                        self.move_to(Direction::Right);
                         self.state = State::A;
                     }
                 },
@@ -117,6 +188,6 @@ impl Machine {
 
 fn main() {
     let mut machine = Machine::new();
-    let checksum = machine.run_till_checksum();
+    let checksum = machine.run();
     println!("{}", checksum);
 }
